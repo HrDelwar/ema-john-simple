04029f1249a61377c719ba98030eaf1ba743b222
HrDelwar <58727109+HrDelwar@users.noreply.github.com>
2021-03-16 21:37:04 +0600

    Delete firebase.config.js

--- src/components/Login/firebase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCE7oajip9NpZo5lcXyretSMYST_Q20d0M",
-    authDomain: "hr-product-d81fd.firebaseapp.com",
-    projectId: "hr-product-d81fd",
-    storageBucket: "hr-product-d81fd.appspot.com",
-    messagingSenderId: "280795600639",
-    appId: "1:280795600639:web:13412852b2c5ca82865eca"
-};
-export default firebaseConfig;
